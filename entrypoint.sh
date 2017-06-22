839f020095c8157dc94d6165a7fdfcda182742f4
Nito Martinez <Nito@Qindel.ES>
2017-06-22 13:39:21 +0200

    Fix permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ cat /dev/null > /etc/rsyslog/00-remote.conf
 [ -n "$UDPLOGHOST" ] && echo "*.* @$UDPLOGHOST" >> /etc/rsyslog/00-remote.conf
 [ -n "$TCPLOGHOST" ] && echo "*.* @@$TCPLOGHOST" >> /etc/rsyslog/00-remote.conf
 [ -n "$UDPLOGHOST" -o -n "$TCPLOGHOST" ] && /usr/sbin/rsyslogd -f /etc/rsyslog.conf
-chown -R named:named /var/cache/bind
+chown -R named:named /var/cache/bind /etc/bind
 /usr/sbin/named -f -c /etc/bind/named.conf -u named
